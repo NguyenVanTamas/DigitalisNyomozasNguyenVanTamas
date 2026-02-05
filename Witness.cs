de40e8618ae806c524bc25f5626ce597f3518e42
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-02-05 15:17:33 +0100

    Witness cs transferred

---
+++ Witness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozasNguyenVanTamas
+{
+	internal class Witness
+	{
+		private Person person;
+		private string testimony;
+		private DateTime testimony_date;
+
+		public Witness(Person person, string testimony, DateTime testimony_date)
+		{
+			this.person = person;
+			this.testimony = testimony;
+			this.testimony_date = testimony_date;
+		}
+
+		public string Testimony { get => testimony; set => testimony = value; }
+		public DateTime Testimony_date { get => testimony_date; set => testimony_date = value; }
+		internal Person Person { get => person; set => person = value; }
+	}
+}
