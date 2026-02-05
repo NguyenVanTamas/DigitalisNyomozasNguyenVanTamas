e08ccbc5a8662ab94e86fb266d76c615b6cdbd80
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-02-05 15:12:03 +0100

    User cs transferred

---
+++ User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalisNyomozasNguyenVanTamas
+{
+	internal class User
+	{
+		private string name;
+		private int id;
+		private string role;
+
+		public User(string name, int id, string role)
+		{
+			this.name = name;
+			this.id = id;
+			this.role = role;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Id { get => id; set => id = value; }
+		public string Role { get => role; set => role = value; }
+	}
+}
